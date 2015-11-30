0d50b515014ecfc12c3baa1c93d5823d073ffdb4
Aleksandr Tkachenko <alex.tk.fb@gmail.com>
2015-11-30 16:06:47 +0200

    Added link to suffix-lemmatizer

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ setup(
     name = 'lexicon-deidentifier',
     description = "A wrapper over the Estnltk's Text class, which extracts personally identifiable information (PII) from text.",
     version = '1.0.0',
+    dependency_links = [
+        'https://github.com/estnltk/suffix-lemmatizer/tarball/master#egg=suffix_lemmatizer-1.0.0'
+    ],
     install_requires = [
         'estnltk==1.3', 
         'unicodecsv==0.14.1',
